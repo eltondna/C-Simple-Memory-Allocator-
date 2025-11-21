7d6b0c7da9a9c94a0694caf9650692cc5b40b19d
u8001594 <u8001594@anu.edu.au>
2025-11-22 03:44:51 +0800

    [Feature]: Complete Naive Memory Allocator allocation

--- src/mymalloc.c
+++ src/mymalloc.c
@@ -11,8 +11,136 @@ const size_t kMaxAllocationSize = (512ull << 20) - kMetadataSize;
 // Memory size that is mmapped (256 MB)
 const size_t kMemorySize = (256ull << 20);
 
+Block * memoryList = NULL;
+
+// ! Multiple of 256 MB
+size_t memAlign(size_t chunk, size_t alignment){
+  return (chunk + alignment - 1) & ~(alignment - 1);
+}
+
+// ! Traverse free list (Best fit)
+// ! Minimum Size : kMetadataSize
+void * searchBlock(size_t size){
+  if (memoryList == NULL)
+      return NULL;
+  Block * node  = memoryList;
+  // ! 1. Traverse free list and find best fit blocks
+  Block * best = NULL;
+
+  // ! Linear Time to find best fit
+  while (node){
+      if (node->size >= size){
+          if (best){
+              if (best->size > node->size) 
+                  best = node;
+          }else best = node;
+      }
+      node = node->next;
+  }
+
+  // ! 1. Find Large Enough Blocks
+  if (best){
+    if (best->size > size){
+          // ! Cond 1 : Leftover > Minimum Size (Split)
+          size_t leftover = best->size - size;
+          if (leftover >= (kMinAllocationSize + kMetadataSize)){
+              // ! 1. Copy best node info to the leftover node 
+              Block * nBlock = ((char *)best + size);
+              nBlock->next = best->next;
+              nBlock->prev = best->prev;
+              nBlock->allocated = false;
+              nBlock->size = best->size - size;
+
+              // ! 2. Amend Neighbor reference
+              if (best->next) best->next->prev = nBlock;
+              if (best->prev) best->prev->next = nBlock;
+
+              // ! 3. Set metadata of allocated node
+              best->allocated = true;
+              best->size      = size;
+              best->prev = best->next = NULL;
+
+              // ! If header Node, adjust 
+              if (best == memoryList) {
+                  memoryList = nBlock;
+                  nBlock->prev = NULL;
+              }
+              return (void *)((char *)(best) + kMetadataSize);
+          }
+          // ! Cond 2 : LeftOver < Minimum Size (Allocated)
+          else{
+              // ! Amend Neighbor Reference
+              if (best->next) best->next->prev = best->prev;
+              if (best->prev) best->prev->next = best->next;
+
+              best->allocated = true;
+              best->prev = best->next = NULL;
+
+              // ! If header Node, adjust 
+              if (best == memoryList) {
+                  memoryList = best->next;
+                  memoryList->prev = NULL;
+              }
+              return (void *)((char *)(best) + kMetadataSize);
+          }
+      }
+      // ! Cond 3 : Best Size == Required Size (Perfect)
+      else{
+        // ! Amend Neighbor Reference
+        if (best->next) best->next->prev = best->prev;
+        if (best->prev) best->prev->next = best->next;
+
+        // ! Set metadata of allocated node
+        best->allocated = true;
+        best->prev = best->next = NULL;
+
+        // ! If header Node, adjust 
+        if (best == memoryList) {
+            memoryList = best->next;
+            memoryList->prev = NULL;
+        }
+        return (void *)((char *)(best) + kMetadataSize);
+      }
+    }
+
+  // ! 2. Cant find large enough block -> allocate + search again
+  // ! Either 256MB or 512MB
+  if (size <= kMemorySize){
+      memoryAllocation(kMemorySize);
+  }else {
+      memoryAllocation(kMaxAllocationSize);
+  }
+  return searchBlock(size);
+}
+
+
+// ! Internal function to mmap
+static void memoryAllocation(size_t size){
+      Block * n_alloc     = (Block *) mmap(NULL, kMemorySize, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+      n_alloc->next       = memoryList->prev = NULL;
+      n_alloc->size       = size;
+      n_alloc->allocated  = false;
+      if (memoryList == NULL){
+          memoryList = n_alloc;
+      }else{
+        n_alloc->next = memoryList;
+        memoryList = n_alloc;
+      }
+}
+
 void *my_malloc(size_t size) {
-  return NULL;
+  if (size <= 0 || size < kMinAllocationSize) 
+      return NULL;
+
+  size_t target_size = memAlign(size + kMetadataSize, kAlignment);
+  if (target_size > kMaxAllocationSize)
+      return NULL;
+  if (!memoryList){
+      // ! 1st mmap
+      size_t alloc_size = target_size > kMemorySize ? kMaxAllocationSize : kMemorySize;
+      memoryAllocation(alloc_size);
+  }
+  return searchBlock(target_size);
 }
 
 void my_free(void *ptr) {
